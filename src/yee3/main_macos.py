cec7c1dad1876ab2a0cfe7c1d6f9f868089ec747
tanaga9 <tanaga9@users.noreply.github.com>
2025-02-23 03:04:43 +0900

    Fixed incorrect OSType specification

--- src/yee3/main_macos.py
+++ src/yee3/main_macos.py
@@ -18,7 +18,7 @@ class MacOSFileHandler(NSObject):
                 window = self.windows[0]
                 window.loadImagesFromFolder(imagePath)
             else:
-                window = initialize_image_viewer(imagePath, os_type=OSType.WINDOWS)
+                window = initialize_image_viewer(imagePath, os_type=OSType.MACOS)
                 self.windows.append(window)
         except Exception as e:
             QMessageBox.critical(None, "", str(e))
@@ -30,9 +30,9 @@ def main():
 
     imagePath = sys.argv[1] if len(sys.argv) > 1 else None
     if imagePath:
-        window = initialize_image_viewer(imagePath, os_type=OSType.WINDOWS)
+        window = initialize_image_viewer(imagePath, os_type=OSType.MACOS)
     else:
-        window = initialize_image_viewer(os_type=OSType.WINDOWS)
+        window = initialize_image_viewer(os_type=OSType.MACOS)
 
     delegate.windows.append(window)
     # QMessageBox.information(
--- src/yee3/main_windows.py
+++ src/yee3/main_windows.py
@@ -10,9 +10,9 @@ def main():
 
     imagePath = sys.argv[1] if len(sys.argv) > 1 else None
     if imagePath:
-        initialize_image_viewer(imagePath, os_type=OSType.MACOS)
+        initialize_image_viewer(imagePath, os_type=OSType.WINDOWS)
     else:
-        initialize_image_viewer(os_type=OSType.MACOS)
+        initialize_image_viewer(os_type=OSType.WINDOWS)
 
     sys.exit(app.exec())
 
